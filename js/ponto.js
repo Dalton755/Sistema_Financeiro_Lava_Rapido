b99875dbe42ef1c6dc7e2f8672e804d47951f567
Dalton755 <150269082+Dalton755@users.noreply.github.com>
2026-06-01 18:26:36 -0300

    Update ponto.js

--- js/ponto.js
+++ js/ponto.js
@@ -1,1 +1,33 @@
+window.onload = () => {
 
+  const hoje =
+    new Date()
+      .toISOString()
+      .split('T')[0];
+
+  document
+    .getElementById('data')
+    .value = hoje;
+
+};
+
+async function carregarDados() {
+
+  const data =
+    document
+      .getElementById('data')
+      .value;
+
+  const resultado =
+    await apiGet({
+
+      acao:
+        'funcionarios-data',
+
+      data
+
+    });
+
+  renderizar(resultado);
+
+}
