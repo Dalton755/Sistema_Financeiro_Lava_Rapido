8b67f5aedb73bbd7dbc632650fced4fb12b9537a
Dalton755 <150269082+Dalton755@users.noreply.github.com>
2026-06-05 09:23:16 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,12 @@
 const API_URL =
 'https://script.google.com/macros/s/AKfycbySEPZMoYrilVyZFuSDbPUsBuvF4yliZktBIcaBL1Jm0Zhu_IbL2zo3J7UkwgrhrNfmxA/exec';
+
+const LOJAS = [
+
+  'Shopping Aricanduva',
+
+  'Shopping Central Plaza',
+
+  'Shopping Oeste Plaza'
+
+];
