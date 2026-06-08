15f424a1792b4d27bc7158fe95f01ee6077e1e16
Dalton755 <150269082+Dalton755@users.noreply.github.com>
2026-06-08 20:44:27 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const API_URL =
-'https://script.google.com/macros/s/AKfycbyqCA1JJM1wvUsWH5w2iMS3WhvojL64hJG7RPj2uTs85MxTPJA11DRjwgwEeJesC6O4tg/exec';
+'https://script.google.com/macros/s/AKfycbxjRmty5ua5l0OqZDLs_AbKI-87UjbC71iXp1-ib0eHWTvKxR9gi1Yciw-tFAjt6Xt2SQ/exec';
 
 const LOJAS = [
 
